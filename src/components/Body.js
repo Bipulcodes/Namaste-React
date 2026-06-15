021c25473dd3f8ba25278f569fea1b1db1b23cc4
Bipul Mukherjee <mailofbipul@gmail.com>
2026-06-15 23:44:36 +0530

    added one shimmer component and added one button on the header with the toggle functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,32 +2,37 @@ import ResturantCard from "./ResturantCard";
 import resList from "../../utils/mockData";
 import { useEffect, useState } from "react";
 import { IMAGE_URL } from "../../utils/constants";
-
+import Shimmer from "./Shimmer";
 const Body = () => {
     //state variable are the - Suoeroowerful variables 
-    const [listOfRestaurants, setListOfRestraunt] = useState();
+    const [listOfRestaurants, setListOfRestraunt] = useState([]);
 
     useEffect(()=>{fetchData();},[]);
 
     const fetchData = async () => {
-        const data = await fetch("api/swiggy/dapi/restaurants/list/v5?lat=22.56430&lng=88.36930&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("/api/swiggy/dapi/restaurants/list/v5?lat=22.56430&lng=88.36930&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
         const json = await data.json();
         const restaurantData = json?.data?.cards?.find((item)=> item?.card?.card?.id?.includes("restaurant_grid"))?.card?.card?.gridElements?.infoWithStyle?.restaurants
         setListOfRestraunt(restaurantData);
 
-        console.log(restaurantData);
+       console.log(listOfRestaurants);
         
         
         //console.log(json?.data?.cards?.find((item)=> item?.card?.card?.id?.includes("restaurant_grid"))?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         
     };
 
-    return(
+    //conditional rendering
+    // if(listOfRestaurants.length === 0){
+    //     return <Shimmer />
+    // }
+
+    return listOfRestaurants.length === 0 ? (<Shimmer/>):(
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=>{
-                    const filteredList = listOfRestaurants.filter(
+                    const filteredList = listOfRestaurants.info.filter(
                         (res)=> res.data.avgRating > 4
                     );
                     setListOfRestraunt(filteredList);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../../utils/constants";
 const Header = () => {
+    const [btnNamereact , setbtnNamereact] = useState("Login")
+    //let btnName = "Login"    
     return(
         <div className="header">
             <div className="imagelogo">
@@ -7,6 +10,13 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
+                    
+                    <button className="login" onClick={
+                        ()=>{if(btnNamereact == "Login") {setbtnNamereact("Logout")}
+                            else setbtnNamereact("Login")
+                        //console.log(btnNamereact);
+                        
+                    }}>{btnNamereact}</button>
                     <li>Home</li>
                     <li>About Us</li>
                     <li>Contact Us</li>
